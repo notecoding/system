b683c7ac560de8619387a9e4850f000d50f7ef11
notecoding <63888297+notecoding@users.noreply.github.com>
2025-03-21 12:27:30 +0900

    Create mission.c

---
+++ 0321/namenuminput/mission.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+  char num[100];
+  char name[100]
+  printf("학번 입력:");
+  scanf("%s",num);
+  printf("이름 입력:");
+  scanf("%s",name);
+
+  printf("학번: %s\n 이름: %s\n",num,name);
+  
+  
+}
