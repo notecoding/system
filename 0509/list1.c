0abcc7e955305ed22cd95417defa43c3a198b911
notecoding <63888297+notecoding@users.noreply.github.com>
2025-05-14 09:30:08 +0900

    Update list1.c

--- 0509/list1.c
+++ 0509/list1.c
@@ -13,3 +13,16 @@
  13   structstat st;
  14   char path[BUFSIZ+1];
  15
+16   if (argc== 1)
+ 17      dir= "."; // 현재디렉터리를대상으로
+18   else dir= argv[1];
+ 19
+ 20   if ((dp= opendir(dir)) == NULL) // 디렉터리열기
+21      perror(dir);
+ 22
+ 23   while ((d = readdir(dp)) != NULL) // 각디렉터리엔트리에대해
+24      printf("%s \n", d->d_name); // 파일이름프린트
+25
+ 26   closedir(dp);
+ 27   exit(0);
+ 28 }
