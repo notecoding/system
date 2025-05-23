9c1f0ae4a353e49b0d7d7b1c9caf9bd5db16d156
notecoding <63888297+notecoding@users.noreply.github.com>
2025-05-23 14:34:37 +0900

    Update fork2.c

--- 0523/fork2.c
+++ 0523/fork2.c
@@ -1,14 +1,18 @@
- #include <stdlib.h>
- #include <stdio.h>
- /* 부모 프로세스가 자식프로세스를생성하고서로다른메시지를프린트*/
- int main() 
-{
- int pid;
- pid = fork();
- if (pid ==0) {   // 자식 프로세스
-printf("[Child] : Hello, world pid=%d\n“, getpid());
- }
- else {   // 부모 프로세스
-printf("[Parent] : Hello, world pid=%d\n", getpid());
- }
- }
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/* 부모 프로세스가 자식 프로세스를 생성하고 서로 다른 메시지를 출력 */
+int main(void) {
+    int pid;
+    pid = fork();
+
+    if (pid == 0) {   // 자식 프로세스
+        printf("[Child] : Hello, world pid=%d\n", getpid());
+        exit(0);
+    } else {          // 부모 프로세스
+        printf("[Parent] : Hello, world pid=%d\n", getpid());
+    }
+
+    return 0;
+}
